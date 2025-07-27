bd37c560cd1e0d511dd0a2a2e4a234b2b388e24e
ParthVekariya147 <parthvekariya147@gmail.com>
2025-07-28 00:28:12 +0530

    register page UI

---
+++ src/styles/tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+  
+
+  
