e758a94449a2d4eba0c1e16a9f9cbce9841a21a6
krishajetani812 <krishajetani289@gmail.com>
2025-07-31 22:30:35 +0530

    only change website name and icon

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Helmet = (props) => {
 
-    document.title = "Car Rent Service - " + props.title ;
+    document.title = "Luxora_Rides - " + props.title ;
 
     return <div className='w-100'>
             {props.children}
